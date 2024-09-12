68a2b05e03d73211db7152e1ee6d3fe2c18066c4
Elise Medeiros <79584555+Elise-16-medeiros@users.noreply.github.com>
2024-09-12 12:13:27 -0300

    navbar part 1

---
+++ project-1/src/components/DesktopNavbar.vue
@@ -0,0 +1,61 @@
+<script setup>
+const links = [
+	{ url: "#home", label: "Home" },
+	{ url: "#sobre", label: "Sobre" },
+	{ url: "#massagens", label: "Massagens" },
+	{ url: "#detox", label: "Detox" },
+	{ url: "#contato", label: "Contato" },
+];
+</script>
+
+<template>
+	<nav class="nav_desktop">
+		<a href="/" class="logo">encanto das mãos</a>
+
+		<ul class="list_nav">
+			<li v-for="(link, index) in links" :key="index">
+				<a :href="link.url" target="_self">{{ link.label }}</a>
+			</li>
+		</ul>
+       
+	</nav>
+</template>
+
+<style scoped>
+.nav_desktop {
+	display: flex;
+	justify-content: space-between;
+	justify-items: center;
+	padding: 1.5rem;
+}
+
+a {
+	text-decoration: none;
+	color: var(--text-color);
+}
+
+li {
+	list-style-type: none;
+}
+
+ul {
+	display: flex;
+	flex-direction: row;
+	gap: 0.5rem;
+}
+
+.logo {
+	font-family: "Corinthia", cursive;
+	font-weight: 700;
+	text-transform: capitalize;
+	font-size: 1.8rem;
+}
+
+@media(max-width: 1024px) {
+    .list_nav {
+        display: none;
+    }
+
+    
+}
+</style>
